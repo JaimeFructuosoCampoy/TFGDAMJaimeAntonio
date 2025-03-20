d8a9308f7b0209d6ae48db55ef5706500bf0dfee
JaimeFructuosoCampoy <jaimecampoy80@gmail.com>
2025-03-20 19:13:15 +0100

    Actualizar movimiento jugador

--- TFGDAMJaimeAntonio/Assets/Scripts/PlayerMoveScript.cs
+++ TFGDAMJaimeAntonio/Assets/Scripts/PlayerMoveScript.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour
 {
-    public float MovementSpeed = 3f;
-    public float JumpSpeed = 5f;
-    private Rigidbody2D Rb2D;
     private bool CanJump = false;
     // Start is called before the first frame update
     void Start()
     {
-        Rb2D = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         CheckMovement();
         CheckJump();
@@ -23,25 +20,22 @@ public class PlayerMoveScript : MonoBehaviour
 
     private void CheckMovement()
     {
-        if (Input.GetKey("right") || Input.GetKey("d"))
-            Rb2D.velocity = new Vector2(MovementSpeed, Rb2D.velocity.y);
-        else if (Input.GetKey("left") || Input.GetKey("a"))
-            Rb2D.velocity = new Vector2(-MovementSpeed, Rb2D.velocity.y);
-        else
-            Rb2D.velocity = new Vector2(0, Rb2D.velocity.y);
+        if (Input.GetKey("left"))
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x + -3f * Time.deltaTime, gameObject.transform.position.y);
+        else if (Input.GetKey("right"))
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x + 3f * Time.deltaTime, gameObject.transform.position.y);
     }
 
     private void CheckJump()
     {
-        if (Rb2D.velocity.y <= 0)
+        if (gameObject.transform.position.y <= 0)
         {
             CanJump = true;
         }
-
-        if ((Input.GetKey(KeyCode.Space) || Input.GetKey("up") || Input.GetKey("w")) && CanJump && gameObject.transform.position.y < 10)
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey("up") && CanJump && gameObject.transform.position.y < 10))
         {
-            Rb2D.velocity = new Vector2(Rb2D.velocity.x, JumpSpeed);
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 8f * Time.deltaTime);
         }
-        
+
     }
 }
